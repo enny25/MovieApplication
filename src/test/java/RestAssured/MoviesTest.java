fb43679e97a24169b2528262b768935c1915dbd6
DarkSideMatters <Plamen@10.50.138.90>
2016-11-04 11:55:41 +0100

    added Emil's Tests on the rest

---
+++ src/test/java/RestAssured/MoviesTest.java
@@ -0,0 +1,107 @@
+package RestAssured;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import entity.Movie;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+import rest.Movies;
+
+/**
+ *
+ * @author Emil
+ */
+public class MoviesTest {
+
+    static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public MoviesTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getMovieName method, of class Movies.
+     */
+    @Ignore
+    @Test
+    public void testGetMovieName() throws Exception {
+        // To make this work then change title in getMovieName in Movies.java to Title, with a capital T.
+        Movie asd = new Movie("tt0096895", "batman");
+        Movies instance = new Movies();
+        String expResult = "batman";
+        String result = instance.getMovieName(gson.toJson(asd));
+
+        JsonObject json = new JsonParser().parse(result).getAsJsonObject();
+        String movieTitle = json.get("Title").getAsString();
+
+        assertEquals(expResult, movieTitle);
+
+    }
+
+    /**
+     * Test of getMovieId method, of class Movies.
+     */
+    @Ignore
+    @Test
+    public void testGetMovieId() throws Exception {
+        System.out.println("getMovieId");
+        String jsongString = "";
+        Movies instance = new Movies();
+        String expResult = "";
+        String result = instance.getMovieId(jsongString);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addMovieById method, of class Movies.
+     */
+    @Ignore
+    @Test
+    public void testAddMovieById() throws Exception {
+        System.out.println("addMovieById");
+        String jsonString = "";
+        Movies instance = new Movies();
+        instance.addMovieById(jsonString);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addMoviebyName method, of class Movies.
+     */
+    @Ignore
+    @Test
+    public void testAddMoviebyName() throws Exception {
+        System.out.println("addMoviebyName");
+        String jsonString = "";
+        Movies instance = new Movies();
+        instance.addMoviebyName(jsonString);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
