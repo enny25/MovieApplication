62779923b5964a6b9b0225e47c92bc20eb90b4a5
enny25 <fetahond@gmail.com>
2016-11-24 11:52:58 +0100

    fixed add to personal movie list

--- src/main/java/rest/UserPage.java
+++ src/main/java/rest/UserPage.java
@@ -7,8 +7,11 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import entity.Movie;
 import facades.UserFacade;
+import entity.PersonalMovie;
 import java.util.List;
 import javax.persistence.Persistence;
 import javax.ws.rs.GET;
@@ -46,13 +49,29 @@ public class UserPage {
         return gson.toJson(buddies);
     }
     
-//    @POST
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public String addMovie(String movieJsonStr){
-//        Movie movie = gson.fromJson(movieJsonStr, Movie.class);
-//        Movie newMovie = uFacade.AddToPersonalMovieList(username, movie);
-//        String jsonResult = gson.toJson(newMovie);
-//        return jsonResult;
-//    }
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String addMovie(String jsonString){
+       JsonObject json = new JsonParser().parse(jsonString).getAsJsonObject();
+         String imdbid = json.get("imdbid").getAsString();
+        String title = json.get("title").getAsString();
+        String year = json.get("Year").getAsString();
+        String runtime = json.get("Runtime").getAsString();
+        String genre = json.get("Genre").getAsString();
+        String directors = json.get("Directors").getAsString();
+        String actors = json.get("Actors").getAsString();
+        String plot = json.get("plot").getAsString();
+        String language = json.get("Language").getAsString();
+        String imdbrating = json.get("ImdbRating").getAsString();
+        String poster = json.get("Poster").getAsString();
+        Movie movie = new Movie(imdbid,title,year,runtime,genre,directors,actors,plot,language,imdbrating,poster);
+        String rating = json.get("Rating").getAsString();
+        String status = json.get("Status").getAsString();
+        String username = json.get("Username").getAsString();
+        PersonalMovie pm = new PersonalMovie(movie,Integer.parseInt(status),rating);
+        boolean result = uFacade.addToPersonalMovieList(username, pm);
+        String jsonResult = gson.toJson(result);
+        return jsonResult;
+    }
 }
