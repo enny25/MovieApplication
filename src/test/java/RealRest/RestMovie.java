3d7fc69e64c222b6d60457dff953515ce9a3e05b
Nexao <emilklausen2@hotmail.com>
2016-11-30 12:24:11 +0100

    Working with Derby

---
+++ src/test/java/RealRest/RestMovie.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RealRest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import io.restassured.parsing.Parser;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Emil
+ */
+public class RestMovie {
+    
+    static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public RestMovie() {
+    }
+    
+    
+    @BeforeClass
+    public static void setUpBeforeAll(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 8080;
+        RestAssured.basePath = "/seedMaven/";
+        RestAssured.defaultParser = Parser.JSON;
+    }
+    
+    @Before
+    public void setUp(){
+        
+    }
+    
+    @Test
+    public void serverIsRunning(){
+        given().
+                when().get().
+                then().
+                statusCode(200);
+    }
+    
+    @Test
+    public void testAddMovie(){
+        given().when().get("api/movies/movie/Grease").then().statusCode(200);
+    }
+    
+    
+}
